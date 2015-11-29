ef015e359eaf0f68c543806c155a577e7befb5ac
Bartłomiej Szałach <onegrx@gmail.com>
2015-11-29 21:19:53 +0100

    Implement randomElems(size, min, max) function.

--- src/qsort.erl
+++ src/qsort.erl
@@ -10,7 +10,7 @@
 -author("onegrx").
 
 %% API
--export([qs/1]).
+-export([qs/1, randomElems/3]).
 
 lessThan(List, Arg) -> [X || X <- List, X < Arg].
 grtEqThan(List, Arg) -> [X || X <- List, X >= Arg].
@@ -18,3 +18,6 @@ grtEqThan(List, Arg) -> [X || X <- List, X >= Arg].
 qs([]) ->[];
 qs([Pivot|Tail]) ->
   qs(lessThan(Tail,Pivot)) ++ [Pivot] ++ qs(grtEqThan(Tail,Pivot)).
+
+randomElems(N, Min, Max) ->
+  [random:uniform(Max - Min + 1) + Min - 1 || _ <- lists:seq(1,N)].
